e562d157b4be623fe570527e798b625d6353df34
Tanvir <tanvir@nltvc.com>
2018-04-23 12:33:30 +0800

    Opens Numbers Activity by on click event and changes the activity name

--- app/src/main/java/com/newbieiot/miwok/MainActivity.java
+++ app/src/main/java/com/newbieiot/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.newbieiot.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view) {
+        Intent number = new Intent(this, NumbersActivity.class);
+        startActivity(number);
     }
 }
