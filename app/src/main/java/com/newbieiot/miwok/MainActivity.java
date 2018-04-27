7a1cc719ce988813830f6890f684217783d3bec7
Tanvir <tanvir@nltvc.com>
2018-04-27 11:52:25 +0800

    Uses tabbed layout instead of activity layout

--- app/src/main/java/com/newbieiot/miwok/MainActivity.java
+++ app/src/main/java/com/newbieiot/miwok/MainActivity.java
@@ -1,6 +1,7 @@
 package com.newbieiot.miwok;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,42 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        TextView numbers = findViewById(R.id.numbers);
-        TextView family_members = findViewById(R.id.family);
-        TextView colors = findViewById(R.id.colors);
-        TextView phrases = findViewById(R.id.phrases);
 
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent number = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(number);
-            }
-        });
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
-        family_members.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent family = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(family);
-            }
-        });
+        SimpleFPA adapter = new SimpleFPA(getSupportFragmentManager());
 
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent color = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(color);
-            }
-        });
+        viewPager.setAdapter(adapter);
 
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phrase = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrase);
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/newbieiot/miwok/SimpleFPA.java
@@ -0,0 +1,32 @@
+package com.newbieiot.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFPA extends FragmentPagerAdapter {
+
+    //constructor
+    public SimpleFPA(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
